3bc4a43f0891659e2792e805ca32617fbe91be4b
Sid <siddhata.s.patil@gmail.com>
2016-07-15 15:24:29 -0700

    Update memcpy.cpp

--- memcpy.cpp
+++ memcpy.cpp
@@ -1,31 +1,16 @@
-/*
- ==============================================================================
- Name        : memcpy.cpp
- Author      : Siddhata Patil
- Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
- Description : Implement memcpy using cpp
- ==============================================================================
- */
-#include <iostream>
-#include <bitset>
-#include <math.h>
-using namespace std;
 
 #include <bitset>
 
 void memcpy1(unsigned int* dest,unsigned int* src,int s)
 {
-	 unsigned int *pdest = dest;
-     unsigned int *psrc = src;
-
     for(int index = 0; index <  s; index++)
     {
-        *pdest = *psrc;
-		cout<< *pdest <<endl;
-		bitset<32> x(*pdest);
+        dest = src;
+		cout<< *dest <<endl;
+		bitset<32> x(*dest);
 		cout<<x<<endl;
-        pdest ++;
-        psrc ++;
+        dest ++;
+        src ++;
 
 	}
 }
