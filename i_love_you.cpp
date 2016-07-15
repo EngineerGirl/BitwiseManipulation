52c24e46cced6119e96581eac79aa21cce52fd59
Sid <siddhata.s.patil@gmail.com>
2016-07-15 16:26:15 -0700

    Create i_love_you.cpp

---
+++ i_love_you.cpp
@@ -0,0 +1,25 @@
+/*
+ ==============================================================================
+ Name        : i_love_you.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Description : Geeky way to say I Love you
+ ==============================================================================
+ */
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	int a = 3;
+	int b = 2;
+	int c = 7;
+	cout<< "I "<<(unsigned __int8)a  <<" you "<<(unsigned __int8)b<<endl;
+	cout<<(unsigned __int8)c;
+	cout<<(unsigned __int8)c;
+	cout<<(unsigned __int8)c;
+	cout<<(unsigned __int8)c;
+	system ("pause");
+	return 0;
+}
