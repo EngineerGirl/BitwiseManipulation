5ba87028d9bf0ae06777ef1040d63aba355d73a7
Sid <siddhata.s.patil@gmail.com>
2017-03-04 21:58:02 -0800

    Update memcpy.cpp

--- memcpy.cpp
+++ memcpy.cpp
@@ -36,6 +36,50 @@ int main()
 	system ("pause");
 	return 0;
 }
+
+
+//////////////////////////////////////////Implementation in C////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+#define uint8_t unsigned char
+#define uint16_t unsigned short
+#define uint32_t unsigned int
+
+void memcpy(uint32_t nandAddress, uint32_t bytes, uint8_t *data)
+{
+    int index;
+    for(index = 0; index <  bytes; index++)
+    {
+        uint32_t* adress_of_variable_nandAddress = &nandAddress;
+        adress_of_variable_nandAddress = data;
+        printf( "%c \n",*adress_of_variable_nandAddress);
+        nandAddress + sizeof(unsigned int);
+        data ++;
+
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    printf("begin");
+
+    uint8_t b[5] = {'a','b','c','d','e'};
+    //uint32_t *dest  = (uint32_t *)malloc(sizeof(uint32_t)*5);
+    uint8_t *src = b;
+    uint32_t dest ;
+    uint32_t *nandAddress = &dest;
+    memcpy(dest,5,src);
+    //free(dest);
+
+    printf("end");
+    return 0;
+}
+
+
+
 /*
 Printing out just the destination block as a separate function
 void print1(int* dest, int size)
